451afccfdfd3d8d8bc573aef913e4cf86082dc9a
Alan Orth <alan.orth@gmail.com>
2012-03-09 23:54:41 +0300

    device_galaxytab7p.mk: Device uses HDPI artwork where available

--- device_galaxytab7p.mk
+++ device_galaxytab7p.mk
@@ -19,6 +19,9 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, build/target/product/full.mk)
 
+# Galaxy Tab 7 Plus uses high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_galaxytab7p
 PRODUCT_DEVICE := galaxytab7p
